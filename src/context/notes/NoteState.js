1ab64d39ff7afae3f13aac6e7f22d56c8da101b6
TejasShirsath <tejasshirsath5@gmail.com>
2025-02-12 00:23:39 +0530

    Initialize notes state with sample data for context management

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,23 +2,30 @@ import { useState } from 'react';
 import NoteContext  from './noteContext';
 
 const NoteState =  (props)=>{
-    const s1 = {
-        "name": "Tejas",
-        "class": "5b"
-    }
-    const [state, setState] = useState(s1)
-
-    const update = ()=>{
-        setTimeout(()=>{
-            setState({
-                "name": "Bhejas",
-                "class": "6A"
-            })
-        },1000)
-    }
+    const notesInitial = [
+        {
+          "_id": "67a7a57c47b359ef57afd767",
+          "user": "6799cacb1e12023b3384f533",
+          "title": "Plan for 14feb",
+          "description": "Sleep whole day",
+          "tag": "personal",
+          "date": "2025-02-08T18:42:04.644Z",
+          "__v": 0
+        },
+        {
+          "_id": "67a7a57e47b359ef57afd769",
+          "user": "6799cacb1e12023b3384f533",
+          "title": "Plan for 14feb",
+          "description": "Sleep whole day",
+          "tag": "personal",
+          "date": "2025-02-08T18:42:06.431Z",
+          "__v": 0
+        }
+      ]
+      const [notes, setNotes] = useState(notesInitial)
 
     return(
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
              {props.children}
         </NoteContext.Provider>
     )
