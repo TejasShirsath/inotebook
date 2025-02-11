8bf539ae8d249b092748630698748b127f886c3a
TejasShirsath <tejasshirsath5@gmail.com>
2025-02-12 01:49:34 +0530

    Add multiple sample notes for testing and demonstration purposes

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -20,6 +20,51 @@ const NoteState =  (props)=>{
           "tag": "personal",
           "date": "2025-02-08T18:42:06.431Z",
           "__v": 0
+        },
+        {
+          "_id": "67a7a57e47b359ef57afd769",
+          "user": "6799cacb1e12023b3384f533",
+          "title": "Plan for 14feb",
+          "description": "Sleep whole day",
+          "tag": "personal",
+          "date": "2025-02-08T18:42:06.431Z",
+          "__v": 0
+        },
+        {
+          "_id": "67a7a57e47b359ef57afd769",
+          "user": "6799cacb1e12023b3384f533",
+          "title": "Plan for 14feb",
+          "description": "Sleep whole day",
+          "tag": "personal",
+          "date": "2025-02-08T18:42:06.431Z",
+          "__v": 0
+        },
+        {
+          "_id": "67a7a57e47b359ef57afd769",
+          "user": "6799cacb1e12023b3384f533",
+          "title": "Plan for 14feb",
+          "description": "Sleep whole day",
+          "tag": "personal",
+          "date": "2025-02-08T18:42:06.431Z",
+          "__v": 0
+        },
+        {
+          "_id": "67a7a57e47b359ef57afd769",
+          "user": "6799cacb1e12023b3384f533",
+          "title": "Plan for 14feb",
+          "description": "Sleep whole day",
+          "tag": "personal",
+          "date": "2025-02-08T18:42:06.431Z",
+          "__v": 0
+        },
+        {
+          "_id": "67a7a57e47b359ef57afd769",
+          "user": "6799cacb1e12023b3384f533",
+          "title": "Plan for 14feb",
+          "description": "Sleep whole day",
+          "tag": "personal",
+          "date": "2025-02-08T18:42:06.431Z",
+          "__v": 0
         }
       ]
       const [notes, setNotes] = useState(notesInitial)
