09ad33879a362dad1bdbc7a21d5933a5d0e713ed
TejasShirsath <tejasshirsath5@gmail.com>
2025-02-15 23:57:50 +0530

    Add update functionality to Noteitem component for editing notes

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -4,7 +4,7 @@ import noteContext from "../context/notes/noteContext";
 const Noteitem = (props) => {
   const context = useContext(noteContext);
   const { deleteNote } = context;
-  const { note } = props;
+  const { note, updateNote } = props;
   return (
     <div className="col-md-3">
       <div className="card my-3">
@@ -12,7 +12,7 @@ const Noteitem = (props) => {
           <div className="d-flex align-items-center">
             <h5 className="card-title">{note.title}</h5>
             <i className="mx-2" onClick={()=>{deleteNote(note._id)}}><ion-icon name="trash-outline"></ion-icon></i>
-            <i className="mx-2"><ion-icon name="create-outline"></ion-icon></i>
+            <i className="mx-2" onClick={()=>{updateNote(note)}}><ion-icon name="create-outline"></ion-icon></i>
           </div>
           <p className="card-text">{note.description}</p>
         </div>
